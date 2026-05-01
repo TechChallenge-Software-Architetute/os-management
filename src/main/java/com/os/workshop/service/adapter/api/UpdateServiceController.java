e030449834e1a312cd5f066519df0ced44797642
Giovanni <gmontezano79@gmail.com>
2026-05-01 13:17:18 -0300

    feat: CRUD services

---
+++ src/main/java/com/os/workshop/service/adapter/api/UpdateServiceController.java
@@ -0,0 +1,49 @@
+package com.os.workshop.service.adapter.api;
+
+import com.os.workshop.service.domain.ServiceEntity;
+import com.os.workshop.service.domain.requests.UpdateServiceRequest;
+import com.os.workshop.service.usecases.UpdateServiceUC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/services")
+public class UpdateServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UpdateServiceController.class);
+
+    @Autowired
+    private UpdateServiceUC updateServiceUC;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ServiceEntity> update(
+            @PathVariable UUID id,
+            @RequestBody UpdateServiceRequest request
+    ) {
+        logger.info("Recebida requisicao para atualizar servico. ID: {}", id);
+
+        try {
+            var service = updateServiceUC.process(id, request);
+
+            logger.info("Servico atualizado. Servico: {}", service);
+
+            return ResponseEntity.ok(service);
+        } catch (RuntimeException e) {
+            logger.error("Erro ao atualizar servico. ID: {}. Erro: {}", id, e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            logger.error("Erro inesperado ao atualizar servico. ID: {}. Erro: {}", id, e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
