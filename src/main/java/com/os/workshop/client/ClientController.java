9b08618038312ccd5fa9c4bf9cacda52838fb2c6
Kelvin Gomes <kelvinrg3@gmail.com>
2026-05-01 20:19:37 +0100

    pre authorize example

--- src/main/java/com/os/workshop/client/ClientController.java
+++ src/main/java/com/os/workshop/client/ClientController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,6 +57,7 @@ public class ClientController {
      * @return lista de clientes com HTTP 200
      */
     @GetMapping
+    @PreAuthorize("ROLE_ADMIN")
     public ResponseEntity<List<ClientResponse>> findAll() {
         List<ClientResponse> response = clientService.findAll().stream()
                 .map(ClientResponse::from)
